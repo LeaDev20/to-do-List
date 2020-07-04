56d16fab8cc092b997effa5ffb522a455653be97
LeandroNuñez <leandronuniez14@gmail.com>
2020-07-04 14:09:25 -0300

    listas las funciones crear y eliminar

--- funciones.js
+++ funciones.js
@@ -1,10 +1,23 @@
-//Crear tarea
+//GENERAL
 var lista = document.getElementById("lista");
-var elementLi = document.createElement("li");
-var inputData = document.getElementById("body--input").nodeValue;
-var contentLi = document.createTextNode(inputData);
-elementLi.appendChild(contentLi);
+var input = document.getElementById("body--input");
+var btnAgregar = document.getElementById("body__boton--agregar");
+
+//Crear/agregar y eliminar tarea:
 
 function agregarTarea(){
-    lista.appendChild(elementLi);
+    var tarea = input.value;
+    var nuevaTarea = document.createElement("li");
+    var contenido = document.createTextNode(tarea);
+    if(tarea == ""){
+        input.setAttribute("placeholder", "No pusiste nada")
+        input.className = "error";
+        lista.remove(nuevaTarea);
+    }
+    nuevaTarea.appendChild(contenido);
+    lista.appendChild(nuevaTarea);
+    input.value = "";
+    for(i = 0; i <= lista.children.length -1; i++){
+        lista.children[i].addEventListener("click", lista.children[i].remove);
+    } 
 }
